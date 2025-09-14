602944bb71940261a5f7378b66c671dae7eea946
Michael Kelsey <kelsey@slac.stanford.edu>
2025-09-13 19:15:19 -0700

    Add flushing to imports.py tracing, to monitor log file live.

--- imports.py
+++ imports.py
@@ -6,46 +6,46 @@
 ## 20250912  Add debugging output to isolate lags in Grace processing
 
 from datetime import datetime
-print(datetime.now(),"imports.py")
+print(datetime.now(),"imports.py",flush=True)
 
 from rawio.IO import *
-print(datetime.now(),"rawio done")
+print(datetime.now(),"rawio done",flush=True)
 
 import math
 import matplotlib
 global plt
 import matplotlib.pyplot as plt
-print(datetime.now(),"math, matplotlib (x2) done")
+print(datetime.now(),"math, matplotlib (x2) done",flush=True)
 
 global pd
 import pandas as pd
 pd.set_option("display.max_row", 100)
-print(datetime.now(),"pandas done")
+print(datetime.now(),"pandas done",flush=True)
 
 ###from pylab import *
-print(datetime.now(),"pylab skipped (overwrites datetime?)")
+print(datetime.now(),"pylab skipped (overwrites datetime?)",flush=True)
 
 import scipy.io as sio
 from scipy import interpolate
 from scipy.optimize import curve_fit
-print(datetime.now(),"scipy (x3) done")
+print(datetime.now(),"scipy (x3) done",flush=True)
 
 import ROOT
-print(datetime.now(),"ROOT done")
+print(datetime.now(),"ROOT done",flush=True)
 
 global np
 import numpy as np
-print(datetime.now(),"numpy done")
+print(datetime.now(),"numpy done",flush=True)
 
 from cats.cdataframe import CDataFrame
-print(datetime.now(),"cats done")
+print(datetime.now(),"cats done",flush=True)
 
 import sys
 import os, os.path 
 import fnmatch
 import glob
 from sklearn.metrics import auc
-print(datetime.now(),"system stuff and sklearn done")
+print(datetime.now(),"system stuff and sklearn done",flush=True)
 
 # Additional settings needed for CDMS work
 global CDMS_SUPERSIM
