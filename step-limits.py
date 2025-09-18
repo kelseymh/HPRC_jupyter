d8b21d03a73cafc390e1f56a0bcd00b0efac56ce
Michael H Kelsey <kelsey@tamu.edu>
2025-09-17 22:42:04 -0500

    Further improve plots, to match the ones from stuck_charges.py/ipynb

--- step-limits.py
+++ step-limits.py
@@ -15,6 +15,8 @@ print(datetime.now(),"phonon_eff done",flush=True)
 import numpy as np
 print(datetime.now(),"numpy done",flush=True)
 import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (5,3.75)
+plt.rcParams['figure.autolayout'] = True
 print(datetime.now(),"matplotlib done",flush=True)
 ###from imports import *
 print(datetime.now(),"imports skipped",flush=True)
@@ -22,56 +24,67 @@ print(datetime.now(),"imports skipped",flush=True)
 print(datetime.now(),"Imports completed.",flush=True)
 
 print(datetime.now(),"Using 10keV samples",flush=True)
-data = chargeSteps('data/EPot_highV/10keVER', 'HV100mm', infix='-max*k')
-##data = chargeSteps('data/EPot_highV/10keVER', 'iZIP5', infix='-max*k')
+hv100mm = chargeSteps('data/EPot_highV/10keVER', 'HV100mm', infix='-max*k')
+izip5   = chargeSteps('data/EPot_highV/10keVER', 'iZIP5', infix='-max*k')
 
-print(datetime.now(),"CPU time",flush=True)
-data.CPUvsVoltage()
+for data in (hv100mm,izip5):
+    print(datetime.now(),f"{data.det} CPU time",flush=True)
+    plt.clf()      # Discard any previously created figure
+    plt.figure()
+    data.CPUvsVoltage()
 
-print(datetime.now(),"Collection Efficiency",flush=True)
+    print(datetime.now(),"Collection Efficiency",flush=True)
 
-effset = { "E": { "Volts": [], "Eff": [] },
-           "U": { "Volts": [], "Eff": [] }
-}
+    effset = { "E": { "Volts": [], "Eff": [] },
+               "U": { "Volts": [], "Eff": [] }
+    }
 
-for vtype in ["E","U"]:
-    dset = data.data[vtype]
-    if len(dset)==0: continue
+    for vtype in ["E","U"]:
+        dset = data.data[vtype]
+        if len(dset)==0: continue
 
-    _,ax = plt.subplots(figsize=(4.5,3))
-    ax.set_title(f"{data.det} 10 keV ER with Charge Step Limit")
-    ax.set_xlabel("Collection Efficiency (PhononE/Eexpected)")
-    ax.set_ylabel("Events / 0.01%")
+        plt.clf()      # Discard any previously created figure
+        plt.figure()
+        plt.title(data.Title(vtype)+"\nwith 50M Charge Step Limit")
+        plt.xlabel("Collection Efficiency (PhononE/Eexpected)")
+        plt.ylabel("Events / 0.1%")
+        
+        for v in data.voltage:
+            if v not in dset: continue
+            
+            print(datetime.now(),f" loading {v}V{vtype} files...",flush=True)
+            geom,events,hits = phonon_eff.load(dset[v])
+            lbl = f"{v}V"
+            plt.hist(events["PhEff"],bins=60,range=(0.95,1.01),
+                     label=lbl,alpha=0.3)
+            
+            effhist,bins = np.histogram(events["PhEff"],bins=60,
+                                        range=(0.95,1.01))
+            imax = np.argmax(effhist)
+            effset[vtype]["Eff"].append((bins[imax]+bins[imax+1])/2.)
+            effset[vtype]["Volts"].append(v)
+            
+            geom=None	# Avoid memory leaks
+            events=None
+            hits=None
+            
+        plt.legend(loc="upper left")
+        plt.savefig(data.Filename("Efficiency",vtype))
+        # End loop over data sets
 
-    for v in data.voltage:
-        if v not in dset: continue
+    plt.clf()      # Discard any previously created figure
+    plt.figure()
 
-        print(datetime.now(),f" loading {v}V{vtype} files...",flush=True)
-        geom,events,hits = phonon_eff.load(dset[v])
-        lbl = f"{v}V"
-        ax.hist(events["PhEff"],bins=150,range=(0.9,1.05),label=lbl,alpha=0.3)
+    for vtype in effset.keys():
+        plt.scatter(effset[vtype]["Volts"],effset[vtype]["Eff"],
+                    label=data.Title(vtype))
 
-        effhist,bins = np.histogram(events["PhEff"],bins=150,range=(0.9,1.05))
-        imax = np.argmax(effhist)
-        effset[vtype]["Eff"].append((bins[imax]+bins[imax+1])/2.)
-        effset[vtype]["Volts"].append(v)
+    plt.title(f"{data.det} 10 keV ER Efficiency\nwith Charge Step Limit")
+    plt.xlabel("Voltage [V]")
+    plt.ylabel("Collection Efficiency (mode)")
+    plt.ylim(0.94,1.02)
+    plt.legend(loc="lower left")
+    plt.savefig(data.datadir+f"/Efficiency-Voltage_{data.det}.png")
+    # End loop over detector types
 
-        geom=None	# Avoid memory leaks
-        events=None
-        hits=None
-
-    ax.legend(loc="upper left")
-    plt.savefig(data.Filename("Efficiency",vtype))
-# End loop over data sets
-
-_,ax = plt.subplots(figsize=(4.5,3))
-ax.set_title("10 keV ER Efficiency with Charge Step Limit")
-ax.set_xlabel("Voltage [V]")
-ax.set_ylabel("Collection Efficiency (mode)")
-
-for vtype in effset.keys():
-    ax.scatter(effset[vtype]["Volts"],effset[vtype]["Eff"],
-               label=data.Title(vtype))
-
-ax.legend()
-plt.savefig(data.datadir+f"/Efficiency-Voltage_{data.det}.png")
+### TO DO:  Combine hv100mm and izip5 into single set of efficiency plots
